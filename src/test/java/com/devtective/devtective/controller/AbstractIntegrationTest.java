14218ee8f2644aaf6588fbe85225da3ef034956b
Jonas <mechamojonas@gmail.com>
2025-09-03 23:09:04 -0300

    fix container config

--- src/test/java/com/devtective/devtective/controller/AbstractIntegrationTest.java
+++ src/test/java/com/devtective/devtective/controller/AbstractIntegrationTest.java
@@ -11,22 +11,22 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 // src/test/java/.../AbstractIntegrationTest.java
-@Testcontainers
+//@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 public abstract class AbstractIntegrationTest {
 
-    @Container @ServiceConnection
+    //@Container @ServiceConnection
     protected static PostgreSQLContainer<?> pg =
             new PostgreSQLContainer<>("postgres:15-alpine");
 
     //static { pg.start(); }
-    //@DynamicPropertySource
-    //static void registerProps(DynamicPropertyRegistry r) {
-        //if (!pg.isRunning()) pg.start();
-        //r.add("spring.datasource.url", pg::getJdbcUrl);
-        //r.add("spring.datasource.username", pg::getUsername);
-        //r.add("spring.datasource.password", pg::getPassword);
-    //}
+    @DynamicPropertySource
+    static void registerProps(DynamicPropertyRegistry r) {
+        if (!pg.isRunning()) pg.start();
+        r.add("spring.datasource.url", pg::getJdbcUrl);
+        r.add("spring.datasource.username", pg::getUsername);
+        r.add("spring.datasource.password", pg::getPassword);
+    }
 }
